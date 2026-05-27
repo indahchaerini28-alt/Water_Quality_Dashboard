1d79b3c37020031953726fe627db2bb5892347f7
indahchaerini28-alt <indahchaerini28@gmail.com>
2026-05-27 19:05:55 +0700

    Add files via upload

---
+++ js/server.js
@@ -0,0 +1,160 @@
+const { WebSocketServer } = require('ws');
+
+const http = require('http');
+
+const admin = require('firebase-admin');
+
+/* ================= FIREBASE ================= */
+
+const serviceAccount =
+require('./serviceAccountKey.json');
+
+admin.initializeApp({
+
+  credential:
+  admin.credential.cert(serviceAccount),
+
+  databaseURL:
+  "https://water-quality-dashboard-b6ac0-default-rtdb.asia-southeast1.firebasedatabase.app"
+
+});
+
+const db = admin.database();
+
+/* ================= HTTP SERVER ================= */
+
+const server = http.createServer((req, res) => {
+
+  res.writeHead(200, {
+
+    'Content-Type': 'text/plain'
+  });
+
+  res.end('WebSocket Server Running');
+
+});
+
+/* ================= WEBSOCKET SERVER ================= */
+
+const wss = new WebSocketServer({
+
+  server
+
+});
+
+/* ================= CONNECTION ================= */
+
+wss.on('connection', (ws, req) => {
+
+  console.log(
+  'CLIENT CONNECTED:',
+  req.socket.remoteAddress
+  );
+
+  /* ================= RECEIVE DATA ================= */
+
+  ws.on('message', async(message) => {
+
+    try {
+
+      /* ================= PARSE JSON ================= */
+
+      const data =
+      JSON.parse(message.toString());
+
+      console.log(
+      'DATA RECEIVED:',
+      data
+      );
+
+
+      /* ================= SAVE FIREBASE ================= */
+
+      await db.ref("history").push({
+
+        timestamp:
+        data.timestamp ||
+
+        new Date()
+        .toLocaleString(),
+
+        ph:
+        data.ph || 0,
+
+        turbidity:
+        data.turbidity || 0,
+
+        salinity:
+        data.salinity || 0,
+
+        status:
+        data.status || "-",
+
+        ph_category:
+        data.ph_category || "-",
+
+        turbidity_category:
+        data.turbidity_category || "-",
+
+        salinity_category:
+        data.salinity_category || "-",
+
+        server_time:
+        Date.now()
+
+      });
+
+      /* ================= BROADCAST ================= */
+
+      wss.clients.forEach((client) => {
+
+        if(client.readyState === 1){
+
+          client.send(
+          JSON.stringify({
+
+            ...data
+
+          })
+          );
+        }
+
+      });
+
+    }
+
+    catch(err){
+
+      console.log(
+      'ERROR:',
+      err.message
+      );
+    }
+
+  });
+
+  /* ================= CLOSE ================= */
+
+  ws.on('close', () => {
+
+    console.log(
+    'CLIENT DISCONNECTED'
+    );
+
+  });
+
+});
+
+/* ================= START SERVER ================= */
+
+server.listen(8080, '0.0.0.0', () => {
+
+  console.log('================================');
+
+  console.log('SERVER RUNNING');
+
+  console.log('ws://10.144.26.233:8080');
+
+  console.log('================================');
+
+});
